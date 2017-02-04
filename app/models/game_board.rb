b2583bee4715a7c0ed1dba5821e8810e8ebbe2c6
Aileen Rowan <aileen.s.rowan@gmail.com>
2017-02-04 16:02:50 -0500

    tested spot_at method

--- app/models/game_board.rb
+++ app/models/game_board.rb
@@ -15,18 +15,6 @@ class GameBoard < ApplicationRecord
     end
   end
 
-  def row_one_spots
-    board_spots.select { |spot| spot.y_pos == 1 }
-  end
-
-  def row_two_spots
-    board_spots.select { |spot| spot.y_pos == 2 }
-  end
-
-  def row_three_spots
-    board_spots.select { |spot| spot.y_pos == 3 }
-  end
-
   def spots_at(x, y)
     board_spots.select { |spot| spot.x_pos == x && spot.y_pos == y }.sort_by{ |spot| spot.size }
   end
--- spec/models/game_board_spec.rb
+++ spec/models/game_board_spec.rb
@@ -9,4 +9,17 @@ RSpec.describe GameBoard do
     end
 
   end
+
+  describe "game board" do
+    it "can retrieve all spots at a certain position" do
+      my_game = Game.create(number_players: 2)
+      board = my_game.game_board
+      bottom_left_spots = board.spots_at(1, 1)
+      expect(bottom_left_spots.size).to eq(3)
+      sizes = bottom_left_spots.map { |spot| spot.size }
+      expect(sizes).to match_array(["sm", "md", "lg"])
+      expect(bottom_left_spots.first.x_pos).to eq(1)
+      expect(bottom_left_spots.first.y_pos).to eq(1)
+    end
+  end
 end
